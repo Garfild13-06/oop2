40f84cb39d593c00fcc2571989fb8029ffe5faa7
Garfild13-06 <Garfild13-06@yandex.com>
2023-03-30 12:45:16 +0300

    Task 2 done

--- main.py
+++ main.py
@@ -7,12 +7,33 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -23,13 +44,9 @@ class Mentor:
         else:
             return 'Ошибка'
 
-
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
+    # def __str__(self):
+    #     res = f"Имя: {self.name}\nФамилия: {self.surname}"
+    #     return res
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -38,8 +55,14 @@ best_student.courses_in_progress += ['Python']
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+new_reviewer = Reviewer('Seth', 'Dougan')
+new_reviewer.courses_attached += ['Python']
+new_reviewer.rate_hw(best_student, 'Python', 10)
+
+new_lecturer = Lecturer('Alex', 'Griffin')
+new_lecturer.courses_attached += ['Python']
+
+best_student.rate_hw(new_lecturer, 'Python', 5)
 
 print(best_student.grades)
+print(new_lecturer.grades)
